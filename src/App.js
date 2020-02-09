79776ebff898c1242aa1222d6e86a10a63bd6584
mmlait <miia.laitinen@hotmail.com>
2020-02-09 11:56:16 +0200

    add info into user page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from './components/layout/NavBar';
 import Alert from './components/layout/Alert';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import About from './components/pages/About';
 import axios from 'axios';
@@ -11,6 +12,7 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -37,6 +39,17 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  getUser = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios
+      .get(`https://api.github.com/users/${username}?client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   clearUsers = () => this.setState({ users: [], loading: false });
 
   setAlert = (msg, type) => {
@@ -47,6 +60,7 @@ class App extends Component {
   render() {
     const {
       users, 
+      user,
       loading
     } = this.state;
 
@@ -65,10 +79,21 @@ class App extends Component {
                   showClear={users.length > 0 ? true : false}
                   setAlert={this.setAlert}
                 />
-                <Users loading={loading} users={users} />
+                <Users 
+                  loading={loading} 
+                  users={users} 
+                />
               </Fragment>
             )} />
             <Route exact path='/about' component={About} />
+            <Route exact path='/user/:login' render={props => (
+              <User 
+                { ...props } 
+                getUser={this.getUser} 
+                user={user} 
+                loading={loading} 
+              />
+            )} />
           </Switch>
         </div>
       </div>
--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ icon, title}) => {
+const NavBar = ({ icon, title }) => {
   return (
     <nav className="navbar bg-primary">
       <h1>
